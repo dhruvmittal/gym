a4e7a20d479d9a69e1df0001d7ebf6f6896b3126
Dhruv Mittal <dhruvmittal@outlook.com>
2016-05-29 22:37:47 -0400

    allow for choosing common time ranges.

--- app.py
+++ app.py
@@ -8,6 +8,8 @@ import dateutil
 import io
 from bokeh.charts import Line
 from bokeh.embed import components
+#from bokeh.models import SingleIntervalTicker, LinearAxis
+
 
 
 
@@ -31,12 +33,34 @@ def doSomething():
 		file_key = request.form['symbol'] + '.csv'
 		my_api_key = 'W8o1M8Y_wPyXnjs-HQPq'
 		end_date = datetime.date.today()
-		start_date = end_date + dateutil.relativedelta.relativedelta(months=-1)
+		numMonths = -1
+		if request.form['timeframe'] == 'three': numMonths = -3
+		elif request.form['timeframe'] == 'twelve': numMonths = -12
+		start_date = end_date + dateutil.relativedelta.relativedelta(months=numMonths)
 		payload = {'api_key':my_api_key, 'start_date':start_date, 'end_date':end_date, 'column_index':'4'}
 		try:
 			r = requests.get(api_addr + file_key, params=payload)
 			closes_df = pd.read_csv(io.StringIO(r.content.decode('utf-8'))).sort_index(by='Date')
-			plot = Line(closes_df, x='Date', y='Close', title='Close v. Date for ' + request.form['symbol'])
+			plot = Line(closes_df, x='Date', y='Close', title='Close v. Date for ' + request.form['symbol'], 
+			plot_width=1000)#, x_axis_type=None)
+# Try to figure out how to change the ticker size on the x axis someday when my internet works. 
+			if numMonths==1:
+				tick_size_big=1
+				tick_size_small=0
+				dnt = 31
+			elif numMonths==3:
+				tick_size_big=12
+				tick_size_small=7
+				dnt = 12
+			elif numMonths==12:
+				tick_size_big=12
+				tick_size_small=4
+				dnt = 12
+			#plot.models.ticker.desired_num_ticks = dnt
+			#ticker = SingleIntervalTicker(interval=tick_size_big, num_minor_ticks=tick_size_small)
+			#xaxis = LinearAxis(ticker=ticker)
+			#plot.add_layout(xaxis, 'below')
+
 			script, div = components(plot)
 			return result(request.form['symbol'], script, div)
 		except: 
