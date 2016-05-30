f9504ec45c2e9fa363bd6613cacc5ab19e54e939
Dhruv Mittal <dhruvmittal@outlook.com>
2016-05-29 20:30:12 -0400

    did a lot of stuff, should be done probably

--- app.py
+++ app.py
@@ -1,4 +1,15 @@
 from flask import Flask, render_template, request, redirect
+import pandas as pd
+import numpy as np
+import requests 
+import simplejson
+import datetime
+import dateutil
+import io
+from bokeh.charts import Line
+from bokeh.embed import components
+
+
 
 app = Flask(__name__)
 
@@ -9,6 +20,38 @@ def main():
 @app.route('/index')
 def index():
   return render_template('index.html')
+
+@app.route('/index', methods=['POST'])
+def doSomething():
+  if request.form['symbol']:
+	if request.method == 'POST':
+		print 'Got POST request that looks like ' + request.form['symbol']
+		#api_addr = 'https://www.quandl.com/data/WIKI/'
+		api_addr = 'https://www.quandl.com/api/v3/datasets/WIKI/'
+		file_key = request.form['symbol'] + '.csv'
+		my_api_key = 'W8o1M8Y_wPyXnjs-HQPq'
+		end_date = datetime.date.today()
+		start_date = end_date + dateutil.relativedelta.relativedelta(months=-1)
+		payload = {'api_key':my_api_key, 'start_date':start_date, 'end_date':end_date, 'column_index':'4'}
+		try:
+			r = requests.get(api_addr + file_key, params=payload)
+			closes_df = pd.read_csv(io.StringIO(r.content.decode('utf-8')))
+			plot = Line(closes_df, x='Date', y='Close', title='Close v. Date for ' + request.form['symbol'])
+			script, div = components(plot)
+			return result(request.form['symbol'], script, div)
+		except: 
+			return incorrect_code()
+	else:
+		return 'request method was a GET apparently' 
+
+@app.route('/err')
+def incorrect_code():
+	return render_template('err.html')
+
+
+@app.route('/result/<tickerSymbol>')
+def result(tickerSymbol, bokeh_script, bokeh_div):
+  return render_template('result.html', symbol=tickerSymbol, b_s_=bokeh_script, b_d_=bokeh_div)
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0',port=33507)
